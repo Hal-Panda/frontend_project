9c0fea02bfb5fae6797c1e42fede6b8411f13ecf
XH <923204938@qq.com>
2022-01-06 09:49:27 +0800

    底部主导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<!--  <div id="nav">-->
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </div>-->
+<!--  <router-view/>-->
+  <mainGuide></mainGuide>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+require("./assets/css/reset.css")
+import mainGuide from "./views/mainGuide.vue"
+export default {
+  name: 'App',
+  components: {
+    mainGuide
+  }
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style  lang="less">
+html{
+  background-color: white;
+  font-size: 100vw/750 *50;
 }
 </style>
